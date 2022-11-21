b7ae40898136b0b06468ca4d75b100a157062363
sava <savamedvedev@gmail.com>
2022-11-22 05:57:04 +0800

    SECOND-COMMIT

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Container } from "react-bootstrap"
 import {About} from "./pages/About"
 import {Home} from "./pages/Home";
 import {Store} from "./pages/Store";
+import {Navbar} from  "./components/Navbar"
 
 function App() {
     return (
         <>
+            <Navbar/>
             <Container className="mb-4">
                 <Routes>
                     <Route path="/" element={<Home />} />
